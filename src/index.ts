47ccb68e64aaab168cab98b629576755bf3808a8
Wojtek Zrałek <00wojtekz@gmail.com>
2024-05-15 00:28:22 +0200

    🥝 chore: Update root page

--- src/index.ts
+++ src/index.ts
@@ -7,44 +7,19 @@ import { rateLimit } from "elysia-rate-limit";
 import packageJson from "../package.json";
 import "./db";
 import albumCover from "./routes/albumCover";
-import moses from "./routes/moses";
 import johndoe from "./routes/johndoe";
+import moses from "./routes/moses";
 
 const app = new Elysia()
     .use(cors())
-    .use(
-        rateLimit({
-            duration: 60000,
-            max: 100,
-            responseMessage: "Whoa there, slow down. You can only make 100 requests per minute.",
-        }),
-    )
-    .use(
-        swagger({
-            path: "/docs",
-            exclude: ["/", "/docs", "/docs/json"],
-            documentation: {
-                info: {
-                    title: packageJson.name,
-                    version: packageJson.version,
-                    description: packageJson.description,
-                    license: { name: packageJson.license },
-                },
-            },
-        }),
-    )
+    .use(rateLimit({ duration: 60000, max: 100, errorResponse: "Whoa there, slow down. You can only make 100 requests per minute." }))
+    .use(swagger({ path: "/docs", exclude: ["/", "/docs", "/docs/json"], documentation: { info: { title: packageJson.name, version: packageJson.version, description: packageJson.description, license: { name: packageJson.license } } } }))
     .use(html())
     .use(staticPlugin())
     .get("/", () => Bun.file("./public/index.html").text())
     .group("/moses", (app) => app.use(moses))
     .group("/albumCover", (app) => app.use(albumCover))
     .group("/johndoe", (app) => app.use(johndoe))
-    .onError(({ code, set }) => {
-        if (code === "NOT_FOUND") {
-            set.status = 404;
-            return "404 Not Found";
-        }
-    })
     .listen(3000);
 
 console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`);
